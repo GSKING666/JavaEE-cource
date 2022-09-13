3e0958618e2dbcd47e9a355beb2bd61fae72561b
DESKTOP-OQ8N8ON\DELL <1198494046@qq.com>
2022-09-13 19:17:25 +0800

    代码更新

---
+++ assignments/Hello_world/hello-app/src/main/java/com/example/helloapp/HelloWorld.java
@@ -0,0 +1,7 @@
+package com.example.helloapp;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("HELLO,WORLD!");
+    }
+}
---
+++ assignments/Hello_world/hello-app/src/main/java/com/example/helloapp/test.java
@@ -0,0 +1,15 @@
+package com.example.helloapp;
+
+public class test {
+    public static void main(String[] args) {
+        int a=10;
+        int[] arr=new int[]{11,22,33};
+        arr[0]=44;
+        arr[1]=55;
+        arr[2]=66;
+
+        System.out.println(arr[0]);
+        System.out.println(arr[1]);
+        System.out.println(arr[2]);
+    }
+}
